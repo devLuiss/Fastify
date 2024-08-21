0702c32e51584c6b7ceeef25b894f4032f3579a6
Luis henrique pavão <82249306+devLuiss@users.noreply.github.com>
2024-08-20 23:49:18 -0400

    chore: Add Knex migration file for creating documents table

--- src/database.ts
+++ src/database.ts
@@ -1,8 +1,34 @@
-import { knex as setupKnex } from "knex";
-
-export const knex = setupKnex({
+import { Knex, knex as setupKnex } from "knex";
+export const config: Knex.Config = {
   client: "sqlite",
   connection: {
-    filename: "./tmp/app.db",
+    filename: "./db/db.sqlite",
   },
-});
+  useNullAsDefault: true,
+  migrations: {
+    extension: "ts",
+    directory: "./db/migrations",
+  },
+};
+
+export const knex = setupKnex(config);
+
+// import { Knex, knex as setupKnex } from 'knex'
+// import { env } from './env'
+
+// export const config: Knex.Config = {
+//   client: env.DATABASE_CLIENT,
+//   connection:
+//     env.DATABASE_CLIENT === 'sqlite'
+//       ? {
+//           filename: env.DATABASE_URL,
+//         }
+//       : env.DATABASE_URL,
+//   useNullAsDefault: true,
+//   migrations: {
+//     extension: 'ts',
+//     directory: './db/migrations',
+//   },
+// }
+
+// export const knex = setupKnex(config)
